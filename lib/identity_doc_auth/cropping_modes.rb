ac9ef18309a12bac5b19c8f80cfb87cb84192350
Andrew Duthie <andrew.duthie@gsa.gov>
2021-07-19 14:40:26 -0400

    Set cropping mode constants as string

--- lib/identity_doc_auth/cropping_modes.rb
+++ lib/identity_doc_auth/cropping_modes.rb
@@ -1,10 +1,10 @@
 module IdentityDocAuth
   module CroppingModes
     # No cropping is performed (default).
-    NONE = 0
+    NONE = '0'
     # Automatically determine whether cropping is required. Not recommended.
-    AUTOMATIC = 1
+    AUTOMATIC = '1'
     # Cropping is always performed.
-    ALWAYS = 3
+    ALWAYS = '3'
   end
 end
